43576bbc0c70899298b289987ee32ec905b77c91
Leon Meerfeld <leon.meerfeld@gmail.com>
2016-02-26 14:23:09 +0100

    Added some files

---
+++ quiz.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="de">
+  <head>
+  	<link rel="stylesheet" type="text/css" href="/css/quiz-style.css">
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Simple Web Quiz</title>
+    <script>
+    </script>
+  </head>
+  <body>
+  	<div class="header">
+  	</div>
+    <div class="nav">
+    </div>
+  	<div class="main">
+  	</div>
+  	<div class="footer">
+  	</div>
+  </body>
+</html>
