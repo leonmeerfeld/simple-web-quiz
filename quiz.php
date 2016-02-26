b71860af9f5627989c959e443dd6885a1f9693d1
Leon Meerfeld <leon.meerfeld@gmail.com>
2016-02-26 18:21:30 +0100

    Added some stuff

--- quiz.php
+++ quiz.php
@@ -8,14 +8,31 @@
 
     <title>Simple Web Quiz</title>
     <script>
+      function hideCover() {
+        document.getElementsByClassName('quiz-cover')[0].style.visibility='hidden'; 
+      }
     </script>
   </head>
   <body>
   	<div class="header">
   	</div>
-    <div class="nav">
-    </div>
   	<div class="main">
+      <!--
+      min-width: 600px
+      height: 700px
+      -->
+
+      <div class="quiz-header">
+        <h1>This is a quiz!</h1>
+      </div>
+      <div class="quiz-content">
+        <a>bu</a>
+      </div>
+      <div class="quiz-cover">
+        <div class="start-button">
+          <h2 onclick="hideCover()">START</h2>
+        </div>
+      </div>
   	</div>
   	<div class="footer">
   	</div>
